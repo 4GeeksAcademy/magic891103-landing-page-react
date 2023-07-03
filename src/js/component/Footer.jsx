defa2df3ea79ba4ade5a4cf8d0e12eff283cfb13
magic891103 <133174388+magic891103@users.noreply.github.com>
2023-07-03 18:28:37 +0000

    First Try.

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Footer = () => {
+    return (
+        <div className="bg-dark card-footer text-white" style={{ textAlign: "center" }}>
+            Copyright@ Your Website 2023
+        </div>
+    );
+};
+
+export default Footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,20 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import Footer from "./Footer";
+import Card from "./Card";
+import Jumbotron from "./Jumbotron";
+import Navbar from "./Navbar";
 //create your first component
-const Home = () => {
+
+const home = () => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			< Navbar/>
+			< Jumbotron/>
+		<div className="d-flex row justify-content-around">
+		<BootstrapCard/>
+		</div>
+			< Footer/>
 		</div>
 	);
 };
-
-export default Home;
+export default home;
